d659749f6ed1881aecfec76d835fbc903197d412
James Sudbury <james.sudbury@btinternet.com>
2021-04-09 01:43:44 +0100

    same

---
+++ source/Classes/OpenfireJiveProperty.ps1
@@ -0,0 +1,52 @@
+[DscResource()]
+class OpenfireJiveProperty : OpenfirePropertyBase
+{
+    # Return an instance representing the current state of the resource.
+    [OpenfireJiveProperty] Get()
+    {
+        return ([OpenfirePropertyBase] $this).Get()
+    }
+
+    # Set the state to match the properties specified in this resource.
+    [void] Set()
+    {
+        ([OpenfirePropertyBase] $this).Set()
+    }
+
+    # Compare the current state with the desired state.
+    [System.Boolean] Test()
+    {
+        return ([OpenfirePropertyBase] $this).Test()
+    }
+
+    # OVERRIDES
+
+    # Create a new property
+    [void] CreateProperty()
+    {
+        $this.initJiveGlobals()
+        Invoke-StaticJavaMethod -InputObject $this.jiveGlobals -MethodName 'setXMLProperty' -Arguments $this.PropertyName, $this.Value
+    }
+
+    # Read the value of a property
+    [System.String] ReadProperty()
+    {
+        $this.initJiveGlobals()
+        $currentValue = Invoke-StaticJavaMethod -InputObject $this.jiveGlobals -MethodName 'getXMLProperty' -Arguments $this.PropertyName
+        return $currentValue
+    }
+
+    # Update and existing property
+    [void] UpdateProperty()
+    {
+        # Updates and creation follow the same pattern
+        $this.CreateProperty()
+    }
+
+    # remove an existing property
+    [void] DeleteProperty()
+    {
+        $this.initJiveGlobals()
+        Invoke-StaticJavaMethod -InputObject $this.jiveGlobals -MethodName 'deleteXMLProperty' -Arguments $this.PropertyName
+    }
+}
---
+++ source/en-US/OpenfireJiveProperty.strings.psd1
@@ -0,0 +1,12 @@
+<#
+    .SYNOPSIS
+        The localized resource strings in English (en-US) for the
+        resource OpenfireJiveProperty.
+#>
+
+ConvertFrom-StringData @'
+    GetProperty = Retrieving Jive property '{0}'. (OJP0001)
+    CreateProperty = Creating Jive property '{0}' with a value of '{1}'. (OJP0002)
+    SetProperty = Setting Jive property '{0}' to '{1}'. (OJP0003)
+    RemoveProperty = Removing Jive property '{0}'. (OJP0004)
+'@
