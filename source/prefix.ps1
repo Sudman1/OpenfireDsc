b5509fe0c003b681aefaa0272f9cf7caf99a7c50
James Sudbury <james.sudbury@btinternet.com>
2021-04-09 02:14:41 +0100

    reverted change with prefix to load java types

--- source/prefix.ps1
+++ source/prefix.ps1
@@ -3,3 +3,20 @@ $script:dscResourceCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath '
 Import-Module -Name $script:dscResourceCommonModulePath
 
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
+
+# Load IKVM if it's not already loaded
+try
+{
+    [void] [java.lang.class]
+}
+catch
+{
+    # Write-Verbose $this.localizedData.LoadIKVM
+
+    $dlls = Get-ChildItem "$($PSScriptRoot)\ikvm8\lib\" -Filter "*.dll"
+
+    foreach ($file in $dlls)
+    {
+        [system.reflection.assembly]::LoadFile($file.FullName) | Write-Verbose
+    }
+}
