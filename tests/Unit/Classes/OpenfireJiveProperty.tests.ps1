039f90e26d4a61557ca2cdca90a316f18ff317c6
James Sudbury <james.sudbury@btinternet.com>
2021-04-09 01:43:31 +0100

    Initial Jive properties resource created by copying the XM properties resource files.

---
+++ tests/Unit/Classes/OpenfireJiveProperty.tests.ps1
@@ -0,0 +1,286 @@
+$ProjectPath = "$PSScriptRoot\..\..\.." | Convert-Path
+$ProjectName = (Get-ChildItem $ProjectPath\*\*.psd1 | Where-Object {
+        ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
+        $(try
+            {
+                Test-ModuleManifest $_.FullName -ErrorAction Stop
+            }
+            catch
+            {
+                $false
+            }) }
+).BaseName
+
+Import-Module $ProjectName
+
+InModuleScope $ProjectName {
+    Describe OpenfireJiveProperty {
+        Context 'Constructors' {
+            It 'Should not throw an exception when instantiated' {
+                { [OpenfireJiveProperty]::new() } | Should -Not -Throw
+            }
+
+            It 'Has a default or empty constructor' {
+                $instance = [OpenfireJiveProperty]::new()
+                $instance | Should -Not -BeNullOrEmpty
+                $instance.GetType().Name | Should -Be 'OpenfireJiveProperty'
+            }
+        }
+
+        Context 'Type creation' {
+            It 'Should be type named OpenfireJiveProperty' {
+                $instance = [OpenfireJiveProperty]::new()
+                $instance.GetType().Name | Should -Be 'OpenfireJiveProperty'
+            }
+        }
+    }
+
+    Describe "OpenfireJiveProperty\CRUD functions" -Tag 'Crud' {
+        BeforeEach {
+            $script:testInstance = [OpenfireJiveProperty] @{
+                OpenfireHome = "$($pwd.Path)\tests\Unit\TestOpenfireHome"
+                PropertyName = 'purple.people'
+                Value        = 'eater'
+            }
+        }
+
+        It 'Adds a new XML property value' {
+            $script:testInstance.CreateProperty()
+        }
+
+        It 'Gets an XML Property Value' {
+            $script:testInstance.ReadProperty() | Should -Be $script:testInstance.Value
+        }
+
+        It 'Modifies an existing XML property value' {
+            { $script:testInstance.UpdateProperty() } | Should -Not -Throw
+            $script:testInstance.ReadProperty() | Should -Be $script:testInstance.Value
+        }
+
+        It 'Deletes an existing XML property value' {
+            { $script:testInstance.DeleteProperty() } | Should -Not -Throw
+            $script:testInstance.ReadProperty() | Should -BeNullOrEmpty
+        }
+    }
+
+    Describe "OpenfireJiveProperty\Get Method" -Tag 'Get' {
+        BeforeEach {
+            $script:instanceDesiredState = [OpenfireJiveProperty] @{
+                OpenfireHome = 'C:\Program Files\Openfire'
+                PropertyName = 'purple.people'
+                Value        = 'eater'
+                Ensure       = 'Present'
+            }
+
+            $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name CreateProperty -Value {
+                Write-Verbose "Mock value for CreateProperty() called."
+            } -Force
+
+            $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name DeleteProperty -Value {
+                Write-Verbose "Mock value for DeleteProperty() called."
+            } -Force
+        }
+
+        Context "OpenfireJiveProperty\Get\When the configuration is absent" {
+            BeforeEach {
+                # return an empty value to signify a missing value
+                $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name ReadProperty -Value {
+                    Write-Verbose "Mock value '' for ReadProperty() called."
+                    ""
+                } -Force
+            }
+
+            It 'Should return the state as absent' {
+                $script:instanceDesiredState.Get().Ensure | Should -Be 'Absent'
+            }
+
+            It 'Should return the same values as present in properties' {
+                $getMethodResourceResult = $script:instanceDesiredState.Get()
+
+                $getMethodResourceResult.OpenfireHome | Should -Be $script:instanceDesiredState.OpenfireHome
+                $getMethodResourceResult.ConfigFileName | Should -Be $script:instanceDesiredState.ConfigFileName
+                $getMethodResourceResult.PropertyName | Should -Be $script:instanceDesiredState.PropertyName
+                $getMethodResourceResult.Value | Should -Be $script:instanceDesiredState.Value
+            }
+        }
+
+        Context "OpenfireJiveProperty\Get\When the configuration is present" {
+            BeforeEach {
+                # Return the value requested
+                $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name ReadProperty -Value {
+                    Write-Verbose "Mock value '$($script:instanceDesiredState.Value)' for ReadProperty() called."
+                    $script:instanceDesiredState.Value
+                } -Force
+            }
+
+            It 'Should return the state as present' {
+                $script:instanceDesiredState.Get().Ensure | Should -Be 'Present'
+            }
+
+            It 'Should return the same values as present in properties' {
+                $getMethodResourceResult = $script:instanceDesiredState.Get()
+
+                $getMethodResourceResult.OpenfireHome | Should -Be $script:instanceDesiredState.OpenfireHome
+                $getMethodResourceResult.ConfigFileName | Should -Be $script:instanceDesiredState.ConfigFileName
+                $getMethodResourceResult.PropertyName | Should -Be $script:instanceDesiredState.PropertyName
+                $getMethodResourceResult.Value | Should -Be $script:instanceDesiredState.Value
+            }
+        }
+
+    }
+
+    Describe "OpenfireJiveProperty\Test Method" -Tag 'Test' {
+        BeforeEach {
+            $script:instanceDesiredState = [OpenfireJiveProperty] @{
+                OpenfireHome = 'C:\Program Files\Openfire'
+                PropertyName = 'purple.people'
+                Value        = 'eater'
+                Ensure       = 'Present'
+            }
+
+            $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name CreateProperty -Value {
+                Write-Verbose "Mock value for CreateProperty() called."
+            } -Force
+
+            $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name DeleteProperty -Value {
+                Write-Verbose "Mock value for DeleteProperty() called."
+            } -Force
+        }
+
+
+        Context 'OpenfireJiveProperty\Test\When the system is in the desired state' {
+            BeforeEach {
+                # Return the value requested
+                $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name ReadProperty -Value {
+                    Write-Verbose "Mock value '$($script:instanceDesiredState.Value)' for ReadProperty() called."
+                    $script:instanceDesiredState.Value
+                } -Force
+            }
+
+            Context 'OpenfireJiveProperty\Test\When the configuration is absent' {
+                It 'Should return $true' {
+                    $script:instanceDesiredState.Test() | Should -BeTrue
+                }
+            }
+
+            Context 'OpenfireJiveProperty\Test\When the configuration are present' {
+                It 'Should return $true' {
+                    $script:instanceDesiredState.Test() | Should -Be $true
+                }
+            }
+        }
+
+        Context 'OpenfireJiveProperty\Test\When the system is not in the desired state' {
+            BeforeEach {
+                # return an empty value to signify a missing value
+                $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name ReadProperty -Value {
+                    Write-Verbose "Mock value '' for ReadProperty() called."
+                    ""
+                } -Force
+            }
+
+            Context 'OpenfireJiveProperty\Test\When the configuration should be absent' {
+                It 'Should return $false' {
+                    $script:instanceDesiredState.Test() | Should -BeFalse
+                }
+            }
+
+            Context 'When the configuration should be present' {
+                It 'Should return $false' {
+                    $script:instanceDesiredState.Test() | Should -BeFalse
+                }
+            }
+        }
+    }
+
+    Describe "OpenfireJiveProperty\Set Method" -Tag 'Set' {
+        BeforeEach {
+            $script:instanceDesiredState = [OpenfireJiveProperty] @{
+                OpenfireHome = 'C:\Program Files\Openfire'
+                PropertyName = 'purple.people'
+                Value        = 'eater'
+                Ensure       = 'Present'
+            }
+        }
+
+        Context 'When the system is not in the desired state' {
+            BeforeEach {
+                Mock -CommandName Remove-Item -MockWith {}
+                Mock -CommandName New-Item -MockWith {}
+                Mock -CommandName Set-Item -MockWith {}
+
+                # Return an empty value to signify a missing value
+                $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name ReadProperty -Value {
+                    Get-Item .\
+                } -Force
+
+                # Mock the DeleteProperty() method
+                $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name DeleteProperty -Value {
+                    Write-Verbose "Mock for DeleteProperty() called."
+                    Remove-Item .\
+                } -Force
+
+                # Mock the CreateProperty() method
+                $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name CreateProperty -Value {
+                    Write-Verbose "Mock for CreateProperty() called."
+                    New-Item .\
+                } -Force
+
+                # Mock the UpdateProperty() method
+                $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name UpdateProperty -Value {
+                    Write-Verbose "Mock for UpdateProperty() called."
+                    Set-Item .\ -Value "mock"
+                } -Force
+            }
+
+            Context 'When the configuration should be absent' {
+                BeforeEach {
+                    $script:instanceDesiredState.Ensure = 'Absent'
+                    $script:instanceDesiredState.Value = $null
+                }
+
+                It 'Should delete the property' {
+                    Mock -CommandName Get-Item -MockWith {
+                        Write-Verbose "Mock value '$($script:instanceDesiredState.Value)' for ReadProperty() called."
+                        'a value to delete'
+                    }
+                    { $script:instanceDesiredState.Set() } | Should -Not -Throw
+                    Assert-MockCalled -CommandName Remove-Item -Times 1
+                }
+            }
+
+            Context 'When the configuration should be present' {
+                BeforeEach {
+                    $script:instanceDesiredState.Ensure = 'Present'
+                }
+
+                It 'Should create the property' {
+                    Mock -CommandName Get-Item -MockWith {
+                        Write-Verbose "Mock value '' for ReadProperty() called."
+                        ''
+                    }
+                    { $script:instanceDesiredState.Set() } | Should -Not -Throw
+                    Assert-MockCalled -CommandName New-Item -Times 1
+                }
+            }
+
+            Context 'When the configuration is present but has the wrong properties' {
+                BeforeAll {
+                    # return an dummy value to signify an incorrect value
+                    $dummy = 'dummy'
+                    $script:instanceDesiredState | Add-Member -MemberType ScriptMethod -Name ReadProperty -Value {
+                        Write-Verbose "Mock value '$dummy' for ReadProperty() called."
+                        $dummy
+                    } -Force
+                }
+
+                It 'Should update the property' {
+                    { $script:instanceDesiredState.Set() } | Should -Not -Throw
+                    Assert-MockCalled -CommandName Set-Item -Times 1
+                }
+            }
+
+            Assert-VerifiableMock
+        }
+    }
+}
